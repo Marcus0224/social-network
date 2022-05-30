18a976680f1d9343a187adcf6351421845db421c
Marcus0224 <marcstlouis04@gmail.com>
2022-05-30 15:53:13 -0400

    server updated

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.use(require("./routes"));
-
-
-mongoose.set("debug", true);
-
-app.listen(PORT, () => console.log(`Listening 👂 to localhost:${PORT}`));
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
